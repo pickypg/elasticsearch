515dc75e2dc50a0f5e321bcc93536ace1b3fad5a
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-03-20 22:17:56 +0700

    Introduced a hash and direct based terms global ordinals implementations Slightly adjusted the benchmark

--- src/main/java/org/elasticsearch/search/aggregations/bucket/terms/TermsAggregatorFactory.java
+++ src/main/java/org/elasticsearch/search/aggregations/bucket/terms/TermsAggregatorFactory.java
@@ -39,6 +39,8 @@ public class TermsAggregatorFactory extends ValueSourceAggregatorFactory {
     public static final String EXECUTION_HINT_VALUE_MAP = "map";
     public static final String EXECUTION_HINT_VALUE_ORDINALS = "ordinals";
     public static final String EXECUTION_HINT_VALUE_GLOBAL_ORDINALS = "global_ordinals";
+    public static final String EXECUTION_HINT_VALUE_GLOBAL_ORDINALS_HASH = "global_ordinals_hash";
+    public static final String EXECUTION_HINT_VALUE_GLOBAL_ORDINALS_DIRECT = "global_ordinals_direct";
 
     private final InternalOrder order;
     private final int requiredSize;
@@ -114,7 +116,7 @@ public class TermsAggregatorFactory extends ValueSourceAggregatorFactory {
         estimatedBucketCount = Math.min(estimatedBucketCount, 512);
 
         if (valuesSource instanceof BytesValuesSource) {
-            if (executionHint != null && !executionHint.equals(EXECUTION_HINT_VALUE_MAP) && !executionHint.equals(EXECUTION_HINT_VALUE_ORDINALS) && !executionHint.equals(EXECUTION_HINT_VALUE_GLOBAL_ORDINALS)) {
+            if (executionHint != null && !executionHint.equals(EXECUTION_HINT_VALUE_MAP) && !executionHint.equals(EXECUTION_HINT_VALUE_ORDINALS) && !executionHint.startsWith(EXECUTION_HINT_VALUE_GLOBAL_ORDINALS)) {
                 throw new ElasticsearchIllegalArgumentException("execution_hint can only be '" + EXECUTION_HINT_VALUE_MAP + "' or '" + EXECUTION_HINT_VALUE_ORDINALS  + "' or '" + EXECUTION_HINT_VALUE_GLOBAL_ORDINALS + "', not " + executionHint);
             }
             String execution = executionHint;
@@ -134,10 +136,19 @@ public class TermsAggregatorFactory extends ValueSourceAggregatorFactory {
             assert execution != null;
 
             if (execution.equals(EXECUTION_HINT_VALUE_GLOBAL_ORDINALS)) {
+                execution = EXECUTION_HINT_VALUE_GLOBAL_ORDINALS_HASH;
+            }
+
+            if (execution.equals(EXECUTION_HINT_VALUE_GLOBAL_ORDINALS_HASH)) {
                 if (!((BytesValuesSource.WithOrdinals) valuesSource).hasGlobalOrdinals()) {
                     throw new ElasticsearchIllegalArgumentException("execution_hint '" + EXECUTION_HINT_VALUE_GLOBAL_ORDINALS + "' can only be used, if global ordinals have not been disabled");
                 }
-                return new StringTermsAggregator.WithGlobalOrdinals(name, factories, (BytesValuesSource.WithOrdinals) valuesSource, estimatedBucketCount, order, requiredSize, shardSize, minDocCount, aggregationContext, parent);
+                return new StringTermsAggregator.WithGlobalOrdinalsHash(name, factories, (BytesValuesSource.WithOrdinals) valuesSource, estimatedBucketCount, order, requiredSize, shardSize, minDocCount, aggregationContext, parent);
+            } else if (execution.equals(EXECUTION_HINT_VALUE_GLOBAL_ORDINALS_DIRECT)) {
+                if (!((BytesValuesSource.WithOrdinals) valuesSource).hasGlobalOrdinals()) {
+                    throw new ElasticsearchIllegalArgumentException("execution_hint '" + EXECUTION_HINT_VALUE_GLOBAL_ORDINALS + "' can only be used, if global ordinals have not been disabled");
+                }
+                return new StringTermsAggregator.WithGlobalOrdinalsDirect(name, factories, (BytesValuesSource.WithOrdinals) valuesSource, estimatedBucketCount, order, requiredSize, shardSize, minDocCount, aggregationContext, parent);
             } else if (execution.equals(EXECUTION_HINT_VALUE_ORDINALS)) {
                 assert includeExclude == null;
                 return new StringTermsAggregator.WithOrdinals(name, factories, (BytesValuesSource.WithOrdinals) valuesSource, estimatedBucketCount, order, requiredSize, shardSize, minDocCount, aggregationContext, parent);
