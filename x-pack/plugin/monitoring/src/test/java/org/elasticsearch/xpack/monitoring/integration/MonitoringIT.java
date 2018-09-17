87abd32f4efbe87602b28f8ccaf76bd595da9d74
Chris Earle <pickypg@users.noreply.github.com>
2018-09-17 17:19:07 -0400

    exempt check

--- x-pack/plugin/monitoring/src/test/java/org/elasticsearch/xpack/monitoring/integration/MonitoringIT.java
+++ x-pack/plugin/monitoring/src/test/java/org/elasticsearch/xpack/monitoring/integration/MonitoringIT.java
@@ -464,6 +464,11 @@ public class MonitoringIT extends ESSingleNodeTestCase {
                 return;
             }
 
+            // tested in other places; extractValue does not accept wildcards
+            if ("node_stats.shards.*".equals(filter)) {
+                return;
+            }
+
             assertThat(filter + " must not be null in the monitoring document", extractValue(filter, source), notNullValue());
         });
     }
